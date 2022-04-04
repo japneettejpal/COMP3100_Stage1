94f95bb48add75395d99d4ba5b527a097c3282f6
japneettejpal <japneetejpal.sachdev@students.mq.edu.au>
2022-04-05 01:51:23 +1000

    final commit

--- MyClient_Stage1.java
+++ MyClient_Stage1.java
@@ -1,3 +1,4 @@
+
     import java.io.*;
     import java.net.*;
     import java.util.ArrayList;
@@ -12,111 +13,127 @@
       private static int schedule;
       private static ArrayList < String > ServerLargestList = new ArrayList < String > ();
       private static boolean haveJob = false;
+     
+     
+      //commands for the client -server to print
+      private static String recieve = "RCVD: ";
+      private static String first = "HELO\n";
+      private static String second = "REDY\n";
+      private static String third = "OK\n";
+      private static String fourth = "AUTH ";
+      private static String fifth = "NONE";
+      private static String stop = "QUIT\n";
+     
 
       public static void main(String[] args) {
         try {
-          Socket s = new Socket("127.0.0.1", 50000);
-          DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-          BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+          // default IP address and port
+          Socket ClientSocket = new Socket("127.0.0.1", 50000);
+          DataOutputStream outCommunication = new DataOutputStream(ClientSocket.getOutputStream());
+          BufferedReader inCommunication = new BufferedReader(new InputStreamReader(ClientSocket.getInputStream()));
 
-          System.out.println("Target IP: " + s.getInetAddress() + " Target Port: " + s.getPort());
+          System.out.println("Target IP: " + ClientSocket.getInetAddress() + " Target Port: " + ClientSocket.getPort() +"\n");
           System.out.println("Connection Established" );
          
           //sending messages threeway-handshake
-          dout.write(("HELO\n").getBytes());
-          dout.flush();
-          String str = in .readLine();
-          System.out.println(str);
+          outCommunication.write((first).getBytes());
+          outCommunication.flush();
+          String in_Com = inCommunication .readLine();
+          System.out.println(in_Com);
 
           String username = System.getProperty("user.name");
           System.out.println("SENT: AUTH");
-          dout.write(("AUTH " + username + "\n").getBytes());
-          str = in .readLine();
-          System.out.println("RCVD: " + str);
+          outCommunication.write((fourth + username + "\n").getBytes());
+          in_Com = inCommunication .readLine();
+          System.out.println(recieve + in_Com);
 
-          dout.write(("REDY\n").getBytes());
-          dout.flush();
-          str = in .readLine();
+          outCommunication.write((second).getBytes());
+          outCommunication.flush();
+          in_Com = inCommunication .readLine();
 
 
           //while str is not none keep runnning the loop
-          while (!str.equals("NONE")) {
-            System.out.println(str);
-            System.out.println("RCVD: " + str);
-            String[] jobInfo = str.split(" ");
+          while (!in_Com.equals("NONE")) {
+            System.out.println(in_Com);
+            System.out.println(recieve + in_Com);
+            String[] jobInfo = in_Com.split(" ");
             jobID = Integer.parseInt(jobInfo[2]);
 
-           if (jobInfo[0].equals("JOBN")) {
+           if(jobInfo[0].equals("JOBN")){
+           
+              String GETSCAP = "GETS Capable: " + jobInfo[4] + " " + jobInfo[5] + " " + jobInfo[6] + "\n";
+              outCommunication.write(GETSCAP.getBytes());
+              outCommunication.flush();
+             
+              in_Com = inCommunication .readLine();
+              System.out.println(recieve + in_Com);
 
-              dout.write(("GETS Capable: " + jobInfo[4] + " " + jobInfo[5] + " " + jobInfo[6] + "\n").getBytes());
-              dout.flush();
-              str = in .readLine();
-              System.out.println("RCVD: " + str);
+              String[] simulator = in_Com.split(" ");
+              int numSer = Integer.parseInt(simulator[1]);
 
-              String[] Info = str.split(" ");
-              int serverCount = Integer.parseInt(Info[1]);
-
-              dout.write(("OK\n").getBytes());
-              dout.flush();
+              outCommunication.write((third).getBytes());
+              outCommunication.flush();
  
               if(haveJob==true){
-                for (int i=0;i<serverCount;i++){
-                   str=in.readLine();
+                for (int i=0;i<numSer;i++){
+                   in_Com=inCommunication.readLine();
                    }
               }else if(haveJob !=true){              
-                  for (int i = 0; i < serverCount; i++) {
-                  str = in.readLine();
-                  ServerLargestList.add(str);
-                  String serverInfo[] = str.split(" ");
+                  for (int i = 0; i < numSer; i++) {
+                  in_Com = inCommunication.readLine();
+                //adding the largest server to lists
+                  ServerLargestList.add(in_Com);
+                  String sInfo[] = in_Com.split(" ");
                   //finding the largest server
-                  if (Integer.parseInt(serverInfo[4]) > ServerCore) {
-                    ServerType = serverInfo[0];
-                    SerID = Integer.parseInt(serverInfo[1]);
-                    ServerCore = Integer.parseInt(serverInfo[4]);
+                  if (Integer.parseInt(sInfo[4]) > ServerCore) {
+                    ServerType = sInfo[0];
+                    SerID = Integer.parseInt(sInfo[1]);
+                    ServerCore = Integer.parseInt(sInfo[4]);
                   }
                  
                 }
                
               }
+              // Calculates the largest server count in the array list
+              CountLargestServer();
              
-               CountLargestServer();
-             
-              dout.write(("OK\n").getBytes());
-              dout.flush();
-              System.out.println(str);
-              str = in.readLine();
-              System.out.println(str);
-              System.out.println("RCVD: " + str);
+              outCommunication.write((third).getBytes());
+              outCommunication.flush();
+              System.out.println(in_Com);
+              in_Com = inCommunication.readLine();
+              System.out.println(in_Com);
+              System.out.println(recieve + in_Com);
 
               LRR(schedule);
-             
-              dout.write(("SCHD " + jobID + " " + ServerType + " " + SerID + "\n").getBytes());
-              dout.flush();
-              str = in .readLine();
-              System.out.println("RCVD: " + str);
+              String dispatch = "SCHD " + jobID + " " + ServerType + " " + SerID + "\n";
+              outCommunication.write(dispatch.getBytes());
+              outCommunication.flush();
+              in_Com = inCommunication .readLine();
+              System.out.println(recieve + in_Com);
 
-              dout.write(("REDY\n").getBytes());
-              dout.flush();
-              str = in .readLine();
-              System.out.println(str);
+              outCommunication.write((second).getBytes());
+              outCommunication.flush();
+              in_Com = inCommunication .readLine();
+              System.out.println(in_Com);
             } else if (jobInfo[0].equals("JCPL")) {  // if JCPL then send REDY to ds-server
-              dout.write(("REDY\n").getBytes());
-              dout.flush();
-              str = in .readLine();
+              outCommunication.write((second).getBytes());
+              outCommunication.flush();
+              in_Com = inCommunication .readLine();
             }
           }
 
           System.out.println("SENT: QUIT");
-          dout.write(("QUIT\n").getBytes());
-          str = in .readLine();
-          System.out.println("RCVD: " + str);
+          outCommunication.write((stop).getBytes());
+          in_Com = inCommunication .readLine();
+          System.out.println(recieve + in_Com);
 
-          in .close();
-          dout.close();
-          s.close();
+          System.out.println("Connection is terminated\n");
+          inCommunication .close();
+          outCommunication.close();
+          ClientSocket.close();
 
         } catch (Exception e) {
-          System.out.println(e);
+          System.out.println("Something is wrong"+e);
         }
       }
      
@@ -133,9 +150,10 @@
              }
              }
              //to schedule jobs we will find the remainder
-              public static int LRR(int schedule){
-              schedule = jobID % HighestSerCount;
-              SerID = schedule;
-       return schedule;      
+      public static int LRR(int schedule){
+         schedule = jobID % HighestSerCount;
+         SerID = schedule;
+         return schedule;      
       }
     }
+
